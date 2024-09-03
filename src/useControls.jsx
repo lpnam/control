a54a67d3dff8795a7f570b3c183e87db70bbb33b
lpnam <kgzwhite00@gmail.com>
2024-09-04 05:33:45 +0700

    move

--- src/useControls.jsx
+++ src/useControls.jsx
@@ -21,5 +21,50 @@ export const useControls = (vehicalApi, chassicApi) => {
         }
     });
 
+    useEffect(() => {
+
+        if (!vehicalApi || !chassicApi) return;
+
+        if (controls.w){
+            vehicalApi.applyEngineForce(50, 0);
+            vehicalApi.applyEngineForce(50, 1);
+        } else if (controls.s) {
+            vehicalApi.applyEngineForce(-50, 0);
+            vehicalApi.applyEngineForce(-50, 1);
+        } else {
+            vehicalApi.applyEngineForce(0, 0);
+            vehicalApi.applyEngineForce(0, 1);
+        }
+
+        if (controls.a){
+            vehicalApi.setSteeringValue(0.35, 2);
+            vehicalApi.setSteeringValue(0.35, 3);
+        } else if (controls.d) {
+            vehicalApi.setSteeringValue(-0.35, 2);
+            vehicalApi.setSteeringValue(-0.35, 3);
+        } else {
+            for(let i = 0; i < 4; i++) {
+                vehicalApi.setSteeringValue(0, i);
+            }
+        }
+
+        if (controls.x) {
+            vehicalApi.setBrake(80, 0);
+            vehicalApi.setBrake(80, 1);
+        } else {
+            vehicalApi.setBrake(0, 0);
+            vehicalApi.setBrake(0, 1);
+        }
+
+        if (controls.r) {
+            chassicApi.position.set(-1.5, 0.5, 3);
+            chassicApi.rotation.set(0, 0, 0);
+            chassicApi.velocity.set(0, 0, 0);
+            chassicApi.angularVelocity.set(0, 0, 0);
+        }
+
+    }, [controls]);
+
+    return controls;
 
 }
